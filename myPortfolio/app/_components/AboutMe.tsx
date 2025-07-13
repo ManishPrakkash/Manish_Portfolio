dd8adbef143b9b976246a13703def4811de03bd1
ManishPrakkash <manishprakkash@gmail.com>
2025-07-14 00:52:29 +0530

    Initial State

---
+++ myPortfolio/app/_components/AboutMe.tsx
@@ -0,0 +1,97 @@
+'use client';
+import { useGSAP } from '@gsap/react';
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/all';
+import React from 'react';
+
+gsap.registerPlugin(ScrollTrigger, useGSAP);
+
+const AboutMe = () => {
+    const container = React.useRef<HTMLDivElement>(null);
+
+    useGSAP(
+        () => {
+            const tl = gsap.timeline({
+                scrollTrigger: {
+                    id: 'about-me-in',
+                    trigger: container.current,
+                    start: 'top 70%',
+                    end: 'bottom bottom',
+                    scrub: 0.5,
+                },
+            });
+
+            tl.from('.slide-up-and-fade', {
+                y: 150,
+                opacity: 0,
+                stagger: 0.05,
+            });
+        },
+        { scope: container },
+    );
+
+    useGSAP(
+        () => {
+            const tl = gsap.timeline({
+                scrollTrigger: {
+                    id: 'about-me-out',
+                    trigger: container.current,
+                    start: 'bottom 50%',
+                    end: 'bottom 10%',
+                    scrub: 0.5,
+                },
+            });
+
+            tl.to('.slide-up-and-fade', {
+                y: -150,
+                opacity: 0,
+                stagger: 0.02,
+            });
+        },
+        { scope: container },
+    );
+
+    return (
+        <section className="pb-section" id="about-me">
+            <div className="container" ref={container}>
+                <h2 className="text-4xl md:text-6xl font-thin mb-20 slide-up-and-fade">
+                    I believe in a user centered design approach, ensuring that
+                    every project I work on is tailored to meet the specific
+                    needs of its users.
+                </h2>
+
+                <p className="pb-3 border-b text-muted-foreground slide-up-and-fade">
+                    This is me.
+                </p>
+
+                <div className="grid md:grid-cols-12 mt-9">
+                    <div className="md:col-span-5">
+                        <p className="text-5xl slide-up-and-fade">
+                            Hi, I&apos;m Manish Prakkash
+                        </p>
+                    </div>
+                    <div className="md:col-span-7">
+                        <div className="text-lg text-muted-foreground max-w-[450px]">
+                            <p className="slide-up-and-fade">
+                                I&apos;m I'm a React developer and a 2nd-year CSE student, passionate about building
+                                 seamless, responsive, and user-focused web applications. I specialize in creating 
+                                 intuitive interfaces using React while also expanding my skills in backend development 
+                                 through the MERN stack.
+
+
+                            </p>
+                            <p className="mt-3 slide-up-and-fade">
+                            My goal is to deliver scalable, high-performance solutions that align with 
+                            both user needs and business objectives. With a strong focus on performance, 
+                            accessibility, and clean code architecture, I strive to create full-stack applications
+                             that are not only functional but also impactful.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default AboutMe;
