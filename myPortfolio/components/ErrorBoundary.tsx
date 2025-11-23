06125055d7bee5ec2c4126f42b85e8c235eef6de
ManishPrakkash <manishprakkash@gmail.com>
2025-11-23 19:37:57 +0530

    Fix parameter type in getDerivedStateFromError method in ErrorBoundary component

--- myPortfolio/components/ErrorBoundary.tsx
+++ myPortfolio/components/ErrorBoundary.tsx
@@ -14,7 +14,7 @@ class ErrorBoundary extends Component<Props, State> {
         hasError: false,
     };
 
-    public static getDerivedStateFromError(): State {
+    public static getDerivedStateFromError(_: Error): State {
         return { hasError: true };
     }
 
