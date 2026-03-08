56c4020bb183702f5ca5bd59102c494d46c0dc83
Vercel <vercel[bot]@users.noreply.github.com>
2026-03-08 16:37:53 +0000

    Add Vercel Web Analytics to Next.js
    This is an automated pull request generated by Vercel to help install [Web Analytics](https://vercel.com/docs/analytics) in your application.
    
    Co-authored-by: Vercel <vercel[bot]@users.noreply.github.com>

--- myPortfolio/app/layout.tsx
+++ myPortfolio/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Anton, Roboto_Flex } from 'next/font/google';
 import { ReactLenis } from 'lenis/react';
 import { GoogleAnalytics } from '@next/third-parties/google';
+import { Analytics } from '@vercel/analytics/next';
 
 import 'lenis/dist/lenis.css';
 import './globals.css';
@@ -129,6 +130,7 @@ export default function RootLayout({
                     <Preloader />
                     <StickyEmail />
                 </ReactLenis>
+                <Analytics />
             </body>
             <GoogleAnalytics gaId="G-MBGQG95ZVK" />
         </html>
